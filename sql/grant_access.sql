3d81e0204a2bb35ec0e40363d111a1b64ba6ad89
Lioman <dev@lioman.de>
2016-03-15 17:13:34 +0100

    remove old stuff

--- sql/grant_access.sql
+++
@@ -1,6 +0,0 @@
-USE mysql;
-GRANT ALL ON *.* to 'root'@'%' IDENTIFIED BY '123';
-USE db;
-GRANT ALL ON *.* to 'root'@'%' IDENTIFIED BY '123';
-GRANT ALL ON *.* to 'db'@'%' IDENTIFIED BY '123';
-FLUSH PRIVILEGES; 
